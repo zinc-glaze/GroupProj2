f97d53c77f700575689c67b34fe6d2486d16f202
Zinc Glaze <zekegraves@gmail.com>
2019-01-12 07:48:38 -0500

    cleaned up server.js

--- server.js
+++ server.js
@@ -1,38 +1,33 @@
+//Do I need this?
 require("dotenv").config();
+
+//Import express and set port
 var express = require("express");
-var exphbs = require("express-handlebars");
-
-var db = require("./models");
-
 var app = express();
-
-var mysql = require('mysql');
-var wrap = require('word-wrap');
-var Table = require('cli-table');
-var inquirer = require('inquirer');
-var colors = require('colors');
-
-
 var PORT = process.env.PORT || 3000;
 
-// Middleware
-app.use(express.urlencoded({ extended: false }));
+//Require models for syncing
+var db = require("./models");
+
+// Parse urlencoded requests
+app.use(express.urlencoded({ extended: true }));
+//Parse JSON
 app.use(express.json());
+
+//Serve static content from "public" directory
 app.use(express.static("public"));
 
-// Handlebars
-app.engine(
-  "handlebars",
-  exphbs({
-    defaultLayout: "main"
-  })
-);
+// Import handlebars
+var exphbs = require("express-handlebars");
+//Set handlebars as template engine; set default layout
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
 // Routes
 require("./routes/apiRoutes")(app);
 require("./routes/htmlRoutes")(app);
 
+//Do I need this?
 var syncOptions = { force: false };
 
 // If running a test, set syncOptions.force to true
@@ -41,11 +36,10 @@ if (process.env.NODE_ENV === "test") {
   syncOptions.force = true;
 }
 
-// Starting the server, syncing our models ------------------------------------/
-db.sequelize.sync({force: true}).then(function(){
-	app.listen(PORT, function(){
-	console.log("listenning on http://localhost:" + PORT);
-    
+//Sync sequelize models and start express app
+db.sequelize.sync({force: true}).then(function() {
+	app.listen(PORT, function() {
+    console.log("App now listening at localhost:" + PORT);
   });
 });
 
